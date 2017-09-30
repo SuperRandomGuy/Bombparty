5e4886c16afdfbe6f95b9a0eba97ce75e73bb0ab
SuperRandomGuy <31929866+SuperRandomGuy@users.noreply.github.com>
2017-09-30 12:45:18 +0200

    Update BP Little Overlay.user.js

--- BP Little Overlay.user.js
+++ BP Little Overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BP Little Overlay
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      1.0
 // @description  Little overlay for BombParty
 // @downloadURL  https://github.com/SuperRandomGuy/Bombparty/blob/master/BP%20Little%20Overlay.user.js
 // @author       Nicroc
