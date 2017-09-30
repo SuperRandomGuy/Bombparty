cbc82d23af9c201f4f600dfea7056afa2b7cb151
SuperRandomGuy <31929866+SuperRandomGuy@users.noreply.github.com>
2017-09-30 12:41:25 +0200

    Update BP Little Overlay.user.js

--- BP Little Overlay.user.js
+++ BP Little Overlay.user.js
@@ -22,19 +22,19 @@ var TimeShown = "";
 var parentThing = document.getElementById("SidebarTabButtons");
 var ButtonText1 = createInLine(parentThing,'button');
 if(channel.data.state == "playing"){
-ButtonText1[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"] = 'En attente de la prochaine partie'+TimeShown;
+ButtonText1.innerHTML = 'En attente de la prochaine partie'+TimeShown;
 } else {
-ButtonText1[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"] = 'Partie non commencée'+TimeShown;
+ButtonText1.innerHTML = 'Partie non commencée'+TimeShown;
 }
 parentThing = document.getElementById("AppsBar");
 var ButtonText2 = createInLine(parentThing,'p',"active","TimerID");
-ButtonText2[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"] = "A(0)";
+ButtonText2.innerHTML = "A(0)";
 ButtonText2.align = "center";
 ButtonText2.style.color = "#a0a0a0";
  var option = document.createElement('tr');
  var optionName = document.createElement('td');
  optionName.style.background = "rgb(118, 118, 118);";
-optionName[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"] = "Changer de fond";
+optionName.innerHTML = "Changer de fond";
  var optionSet  = document.createElement('input');
  
  optionSet.setAttribute('type','text');
@@ -54,7 +54,7 @@ optionName[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"
       return false;
     }
   };
- document.getElementById('SettingsTab').children[1].appendChild(option);
+ document.getElementById('SettingsTab').children[1].children[0].appendChild(option);
      document.getElementById("GameCanvas").style.setProperty("background","url(https://media.giphy.com/media/EIyuzZk6IDUMU/source.gif)");
 
 var anim1 = [
@@ -78,6 +78,28 @@ var anim4 = {
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
  var elem = document.getElementById('SidebarTabButtons');
  elem.removeChild(elem.children[2]);
+ var notificationSelect = document.createElement('select');
+ var optionActive = document.createElement('option');
+ optionActive.innerHTML = "Activé";
+ var optionDisActive = document.createElement('option');
+ optionDisActive.innerHTML = "Désactivé";
+ notificationSelect.appendChild(optionActive);
+ notificationSelect.appendChild(optionDisActive);
+ var notificationZone = document.createElement('tr');
+ var notificationText = document.createElement('td');
+ notificationText.innerHTML = "Bruit des notifications";
+ notificationZone.appendChild(notificationText);
+ notificationZone.appendChild(notificationSelect);
+ document.getElementById('SettingsTab').children[1].children[0].appendChild(notificationZone);
+ var sound = new Audio("http://bombparty.sparklinlabs.com/sounds/myTurn.wav");
+ channel.socket.on('chatMessage',function parse(data){
+     var message = data.text;
+    if(notificationSelect.selectedIndex === 0){
+        if(message.toLowerCase().indexOf(window.app.user.displayName.toLowerCase()) !== -1){
+           sound.play();
+          }
+    }
+ });
 app.user.alphaLetter = "a";
 app.user.alphaNumber = 0;
 app.user.alphaIdx = 0;
@@ -89,12 +111,12 @@ channel.socket.on("winWord",function (acteur){
          if(app.user.alphaLetter !== "z"){
              app.user.alphaIdx++;
              app.user.alphaLetter = alphabet[app.user.alphaIdx];
-             ButtonText2[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"] = app.user.alphaLetter.toUpperCase()+"("+app.user.alphaNumber+")";
+             ButtonText2.innerHTML = app.user.alphaLetter.toUpperCase()+"("+app.user.alphaNumber+")";
          } else {
              app.user.alphaIdx = 0;
              app.user.alphaLetter = alphabet[app.user.alphaIdx];
              app.user.alphaNumber++;
-             ButtonText2[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"] = app.user.alphaLetter.toUpperCase()+"("+app.user.alphaNumber+")";
+             ButtonText2.innerHTML = app.user.alphaLetter.toUpperCase()+"("+app.user.alphaNumber+")";
          }
          ButtonText2.animate(anim1,anim2);
          setTimeout(function up2(){ButtonText2.animate (anim3,anim4);},200);
@@ -124,7 +146,7 @@ channel.socket.on("endGame",function end(){
 channel.socket.on("resetBombTimer",function reset(){
 	if(beginning === 0){
 		beginning = 1;
-        ButtonText2[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"] = "A(0)";
+        ButtonText2.innerHTML = "A(0)";
 		Interval = setInterval(function TimeAdvance(){
 	 Totaltime++;
 	 Second++;
@@ -134,7 +156,7 @@ channel.socket.on("resetBombTimer",function reset(){
 	 if(Minute < 10){MinuteShown = "0"+Minute;} else {MinuteShown = Minute;}
 	 if(Minute < 10){HourShown = "0"+Hour;} else {HourShown = Hour;}
 	 TimeShown = HourShown+":"+MinuteShown+":"+SecondShown;
-	ButtonText1[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"] = ' Temps '+TimeShown;
+	ButtonText1.innerHTML = ' Temps '+TimeShown;
 	},1000);
 	}
 });
